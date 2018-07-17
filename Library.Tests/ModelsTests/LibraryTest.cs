27762a373930494a5b669b822ee324030750ff00
Guest User <Guest@epicodus-57.local>
2018-07-17 16:29:53 -0700

    Make all files for Library application.

---
+++ Library.Tests/ModelsTests/LibraryTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Library;
+
+namespace Library.Tests
+{
+  [TestClass]
+  public class LibraryTest
+  {
+    [TestMethod]
+    public void ExampleTest_True()
+    {
+      // eventually your testing code will go here
+    }
+  }
+}
---
+++ Library/Models/Authors.cs
@@ -0,0 +1,12 @@
+namespace Library
+{
+public class Library
+  {
+  public bool ExampleTest(int year)
+    {
+      // logic will go here
+      return false;
+    }
+  }
+}
+  
